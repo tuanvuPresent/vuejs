0cde59d19c4ed1dcb08367bbe5a57d6e621a959c
Vũ Nguyễn Tuấn <tuanvubk96@gmail.com>
2020-02-11 17:13:36 +0700

    add component

---
+++ src/components/ChoiceDemo.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-row align="center">
+        <v-col cols="12">
+            <v-select
+                    :items="items"
+                    :menu-props="{ top: true, offsetY: true }"
+                    label="Label"
+            ></v-select>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+    export default {
+        name: "ChoiceDemo",
+        data: () => ({
+            items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
+        }),
+    }
+</script>
+
+<style scoped>
+</style>
