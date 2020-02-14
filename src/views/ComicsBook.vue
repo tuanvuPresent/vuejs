e16959805e9d963e674728497ac1bfc57d756d07
Vũ Nguyễn Tuấn <tuanvubk96@gmail.com>
2020-02-14 17:30:32 +0700

    add grid componet

--- src/views/ComicsBook.vue
+++ src/views/ComicsBook.vue
@@ -1,39 +1,39 @@
 <template>
-    <div>
-        <v-col>
-            <v-toolbar style="color: #d4f0ff">
-                <v-text-field
-                        v-model="search"
-                        label="Search"
-                        single-line
-                        hide-details
-                        color="" append-icon="mdi-account-search">
-                </v-text-field>
-            </v-toolbar>
-        </v-col>
-        <v-row>
-            <v-col style="background: #7a92b9" :sm="8" v-model="items">
-                <v-row gutters v-for="row in 4" :key="row">
-                    <v-col v-for="col in 2" :key="col">
-                        <v-card @click="click((row-1)*2+col)" height="250">
-                            <v-card-title>
-                                {{items[(row-1)*2+col-1].title}}
-                            </v-card-title>
-                            <v-card-text>
-                                {{items[(row-1)*2+col-1].color}}
-                            </v-card-text>
-                            <v-card-subtitle>
-                                {{items[(row-1)*2+col-1].id}}
-                            </v-card-subtitle>
-                            <v-divider></v-divider>
-                            <v-card-actions>
-                                <v-icon>mdi-home</v-icon>
-                            </v-card-actions>
-                        </v-card>
-                    </v-col>
-                </v-row>
+    <v-layout column>
+        <v-toolbar style="color: #d4f0ff">
+            <v-text-field
+                    v-model="search"
+                    label="Search"
+                    single-line
+                    hide-details
+                    color="" append-icon="mdi-account-search">
+            </v-text-field>
+        </v-toolbar>
+        <v-layout row>
+            <v-col :sm="8" style="background: #7a92b9" v-model="items" v-for="(item,i) in items" :key="item.id">
+                <template>
+                    <v-row gutters v-if="item.id %2 === 0">
+                        <v-col v-for="col in [0,1]" :key="col">
+                            <v-card @click="click(items[i+col])" height="250" v-if="items[i+col]">
+                                <v-card-title>
+                                    {{items[i+col].title}}
+                                </v-card-title>
+                                <v-card-text>
+                                    {{items[i+col].text}}
+                                </v-card-text>
+                                <v-card-subtitle>
+                                    {{items[i+col].color}}
+                                </v-card-subtitle>
+                                <v-divider></v-divider>
+                                <v-card-actions>
+                                    <v-icon>mdi-school</v-icon>
+                                </v-card-actions>
+                            </v-card>
+                        </v-col>
+                    </v-row>
+                </template>
             </v-col>
-            <v-col :sm="4">
+            <v-col :sm="4" style="background: #03b9ad">
                 <v-card>
                     <v-card-title>
                         title
@@ -49,8 +49,8 @@
                     </v-card-actions>
                 </v-card>
             </v-col>
-        </v-row>
-    </div>
+        </v-layout>
+    </v-layout>
 </template>
 
 <script>
@@ -60,19 +60,25 @@
             return {
                 search: '',
                 items: [
-                    {id: 1, title: 'book 1', color: 'red'},
-                    {id: 2, title: 'book 2', color: 'blue'},
-                    {id: 3, title: 'book 3', color: 'green'},
-                    {id: 4, title: 'book 4', color: 'red'},
-                    {id: 5, title: 'book 5', color: 'blue'},
-                    {id: 6, title: 'book 6', color: 'green'},
-                    {id: 7, title: 'book 7', color: 'red'},
-                    {id: 8, title: 'book 8', color: 'blue'},
-                    {id: 9, title: 'book 9', color: 'green'},
+                    {id: 0, title: 'book 0', text: 'read book 0', color: 'red'},
+                    {id: 1, title: 'book 1', text: 'read book 1', color: 'red'},
+                    {id: 2, title: 'book 2', text: 'read book 2', color: 'blue'},
+                    {id: 3, title: 'book 3', text: 'read book 3', color: 'green'},
+                    {id: 4, title: 'book 4', text: 'read book 4', color: 'red'},
+                    {id: 5, title: 'book 5', text: 'read book 5', color: 'blue'},
+                    {id: 6, title: 'book 6', text: 'read book 6', color: 'green'},
+                    {id: 7, title: 'book 7', text: 'read book 7', color: 'red'},
+                    {id: 8, title: 'book 8', text: 'read book 8', color: 'blue'},
+                    {id: 9, title: 'book 9', text: 'read book 9', color: 'green'},
+                    {id: 10, title: 'book 10', text: 'read book 10', color: 'green'},
                 ]
             }
         },
-        methods: {}
+        methods: {
+            click(item) {
+                alert(item.title)
+            }
+        }
     }
 </script>
 
