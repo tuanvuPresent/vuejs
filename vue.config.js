77c5488dde235dd64829296dbed487ef50f135f0
Vũ Nguyễn Tuấn <tuanvubk96@gmail.com>
2020-04-07 11:36:48 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,2 @@
-module.exports ={
-    chainWebpack: config => {
-        const svgRule = config.module.rule("svg");
-
-        svgRule.uses.clear();
-        svgRule
-            .oneOf("external")
-            .resourceQuery(/external/)
-            .use("file-loader")
-            .loader("file-loader")
-            .end()
-            .end()
-            .oneOf("inline")
-            .use("vue-svg-loader")
-            .loader("vue-svg-loader");
-    },
-    transpileDependencies: ["vuetify"]
-};
+module.exports ={};
 
