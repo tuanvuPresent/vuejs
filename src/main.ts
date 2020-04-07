c3edf06bc4eea5535239d65150ad6be641aeb62d
Vũ Nguyễn Tuấn <tuanvubk96@gmail.com>
2020-04-07 11:24:49 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router/routers";
-import vuetify from "./plugins/vuetify";
+// @ts-ignore
+import vuetify from "@/plugins/vuetify";
 import VueCompositionApi from "@vue/composition-api";
 Vue.config.productionTip = false;
 Vue.use(VueCompositionApi);
