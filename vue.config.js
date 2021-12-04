29c4cb5360946c98183fbb3368f81315bb6caff0
tuanvuPresent <tuanvubk96@gmail.com>
2021-12-04 12:19:08 +0000

    refactor code

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ module.exports = {
     "transpileDependencies": [
         "vuetify"
     ],
+    devServer: {
+        disableHostCheck: true
+    },
     chainWebpack: (config) => {
         config.plugin('html').tap((options) => {
             options[0].title = 'Demo Example';
